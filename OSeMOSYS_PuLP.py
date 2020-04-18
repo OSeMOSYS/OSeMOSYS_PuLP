d19cf9ef73800992c522eaea3cc7cff202517891
Dennis3R <dennis.dreier.dd@gmail.com>
2020-04-18 17:59:22 +0200

    __doc__ updated

--- OSeMOSYS_PuLP.py
+++ OSeMOSYS_PuLP.py
@@ -37,27 +37,6 @@ __doc__ = """
 #	4) Review results in the output data file(see script section "SETUP - DATA SOURCES and MONTE CARLO SIMULATION")
 #
 # ============================================================================
-#
-#	The OSeMOSYS modelling framework code was translated from the GNU MathProg code implementation,
-#	version: 8 November 2017 (OSeMOSYS_2017_11_08).
-#
-#	Excerpt of the OSeMOSYS GNU MathProg copyright and license:
-#
-#   Copyright [2010-2015] [OSeMOSYS Forum steering committee see: www.osemosys.org]
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License."
-#
-# ============================================================================
 """
 
 import os
